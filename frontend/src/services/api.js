23d9b3d95077525cbe58215d13b43d83ff28457c
Wesley Erasmus <wesley.erasmus119@gmail.com>
2023-03-18 22:25:13 +0200

    [ api.js ]: Updated the baseURL in the frontend api.js file
    api.js is located in the src folder under services

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // Setting my base url
 const api = axios.create({
-  baseURL: "http://localhost/full_stack_online_store/backend/api/",
+  baseURL: "http://localhost/full_stack_online_store-main/backend/api/",
 });
 
 export default api;
